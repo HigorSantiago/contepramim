15ae1200a7b4b556e3c39219b8ab9a0efa8b5ffa
Higor Santiago <higorsantiago@alu.ufc.br>
2025-07-25 21:50:57 -0300

    Removendo Seção de Comentarios da Pagina

--- src/components/StoryContent/index.jsx
+++ src/components/StoryContent/index.jsx
@@ -1,14 +1,12 @@
 // components/StoryPage/StoryContent.jsx
 import StoryDetail from "./StoryDetail"
-import CommentList from "./Comment/CommentList"
-import mockComments from "./Comment/mockComments"
+
 import styles from "./StoryContent.module.css"
 
 export default function StoryContent({ story }) {
   return (
     <div className={styles.storyContent}>
       <StoryDetail story={story} />
-      <CommentList comments={mockComments} />
     </div>
   )
 }
